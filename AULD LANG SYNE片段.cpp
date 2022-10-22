f68df6dcb29c38b80fd5c8ab73b0fe7943800d36
Hgit-1 <106968861+Hgit-1@users.noreply.github.com>
2022-10-22 10:32:36 +0800

    Add files via upload

---
+++ AULD LANG SYNE片段.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#include<windows.h>
+//include sound track
+#define do0 262
+#define re0 294
+#define mi0 330
+#define fa0 349
+#define so0 392
+#define la0 440
+#define si0 494
+#define do 523
+#define re 587
+#define mi 659
+#define fa 698
+#define so 784
+#define la 880
+#define si 988
+#define do1 1047
+#define re1 1175
+#define mi1 1319
+#define fa1 1397
+#define so1 1568
+#define la1 1760
+#define si1 1967
+#define stp 0
+#define m4 1000
+#define m8 500
+#define m16 250
+#define m2 2000
+#define m1 4000
+using namespace std;
+int main(){
+	Beep(so,m4);
+	Beep(do1,m4);
+	Beep(do1,m4);
+	Beep(do1,m4);
+	Beep(mi1,m4);
+	Beep(re1,1250);
+	Beep(do1,m4);
+	Beep(re1,m4);
+	Beep(mi1,m4);
+	Beep(do1,1250);
+	Beep(do1,m8);
+	Beep(mi1,m4);
+	Beep(so1,m4);
+	Beep(la1,3000);
+	Beep(la1,m4);
+	Beep(so1,1250);
+	Beep(mi1,750);
+	Beep(mi1,m4);
+	Beep(do1,m4);
+	Beep(re1,1250);
+	Beep(do1,750);
+	Beep(re1,m4);
+	Beep(mi1,m4);
+	Beep(do1,1250);
+	Beep(la,750);
+	Beep(la,m4);
+	Beep(so,m4);
+	Beep(do1,m1);
+	//AULD LANG SYNE(PART)
+}
